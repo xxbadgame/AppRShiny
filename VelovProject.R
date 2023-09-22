212de67774805fb4ba4bb95638fa8e3f6a9b4f7c
Imad Farsi <123750555+WaImad@users.noreply.github.com>
2023-09-22 14:49:05 +0200

    Update VelovProject.R

--- VelovProject.R
+++ VelovProject.R
@@ -1,26 +1,39 @@
+install.packages(c("httr","jsonlite"))
+install.packages("RMySQL")
 library(shiny)
 library(httr)
 library(jsonlite)
 library(RMySQL)
 
-contract <- "Lyon"
+#contract <- "Lyon"
 api_key <- "2aa23a6e7a71f92dc1a40fa2d412650e19b794f1" 
 
-base_url <- "https://api.jcdecaux.com/vls/v1/stations"
-url <- paste0(base_url, "?contract=", contract, "&apiKey=", api_key)
+#base_url <- "https://api.jcdecaux.com/vls/v1/stations"
+#url <- paste0(base_url, "?contract=", contract, "&apiKey=", api_key)
 
-response <- GET(url)
+#response <- GET(url)
 
-VelovList <- fromJSON(rawToChar(response$content))
+#VelovList <- fromJSON(rawToChar(response$content))
 
 # il faut manipuler les df pour avoir les bonnes tables au choix pour notre application
 
 con <- dbConnect(MySQL(),
-                  user = "sql11646654",
-                  password = "mj1EKmAPJY",
-                  host = "sql11.freesqldatabase.com",
-                  dbname = "sql11646654")
+                 user = "sql11646654",
+                 password = "mj1EKmAPJY",
+                 host = "sql11.freesqldatabase.com",
+                 dbname = "sql11646654")
 
 
 dbWriteTable(con, "VelovInfo", VelovList)
+dbListTables(con)
+
+#Création de la table ETATS
+
+dataStation = GET("https://api.jcdecaux.com/vls/v3/stations/?contract=lyon&apiKey=ef2b5b3f4ce33805dd75a5192dda54ddd11b5b06")
+
+bddStation <- fromJSON ( rawToChar ( dataStation$content ) , flatten =  TRUE )
+df = data.frame()
+df = rbind(bddStation, unlist(bddStation))
+
+dbWriteTable(con, "VelovETATS", df)
 dbListTables(con)

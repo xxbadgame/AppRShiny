e9588978f798355676c0eb7dcdb3b098e99265d9
Imad Farsi <123750555+WaImad@users.noreply.github.com>
2023-10-20 15:17:18 +0200

    Update server.R

--- server.R
+++ server.R
@@ -4,6 +4,10 @@ library(jsonlite)
 library(RMySQL)
 library(tidygeocoder)
 library(leaflet)
+library(readr)
+library(shinyjs)
+install.packages("ggplot2")
+library(ggplot2)
 
 server <- function(input, output) {
   
@@ -16,12 +20,31 @@ server <- function(input, output) {
   
   # Création du dataframe, à la mise en ligne utiliser la base de données
   VelovList <- fromJSON(rawToChar(GET(url)$content))
-  
+
   
   ## DEBLOQUER au démarrage
-  #VelovList$adresse<-reverse_geo(lat = VelovList$position$lat, long = VelovList$position$lng, method = "osm")
+  #VelovList$adresse<-reverse_geo(lat = VelovList$position$latitude, long = VelovList$position$longitude, method = "osm")
+  VelovList$adresse <- read.csv("VelovAdresses.csv")
+  
   
   # Récupération des données pour les KPI
+  
+  ## Bouton rafrachir
+  
+  observeEvent(input$bouton_refresh, {
+    
+  
+    # Afficher l'animation de chargement au début
+    shinyjs::show("loading_animation")
+    
+    # Exécuter la commande (par exemple, VelovList <- fromJSON(rawToChar(GET(url)$content))
+    VelovList <- fromJSON(rawToChar(GET(url)$content))
+    
+    # Masquer l'animation de chargement après le traitement
+    shinyjs::hide("loading_animation")
+  
+  })
+  
   ### 1 
   placeTotal = mean(VelovList$totalStands$availabilities$stands)
   nombreVeloDispo =  mean(VelovList$totalStands$availabilities$bikes)
@@ -49,9 +72,9 @@ server <- function(input, output) {
   })
   
   
-  ### La carte 
+  ##### La carte ######
   observe({
-    
+  
     num_stations <- input$nombre_stations
     
     # Filtrer les données en fonction du nombre de stations sélectionné
@@ -59,68 +82,70 @@ server <- function(input, output) {
     
     #VelovList$adresse<-reverse_geo(lat = VelovList$position$lat, long = VelovList$position$lng, method = "osm")
     
+    ##### RUTH : FAIRE LA BOUCLE, JE N'AI PAS REUSSI A L'INTEGRER. ATTENTION IL FAUT GARDER LA MAP QUI SE FILTRE, NE PAS CHANGER LES NOMS DE VARIABLES ####
+    
     output$map <- renderLeaflet({
       leaflet() %>%
         addTiles() %>%
         ## a voir si carte marche pas, changer lng et lat
         setView(lng = mean(VelovList_filtered$position$longitude), lat = mean(VelovList_filtered$position$latitude), zoom = 13) %>%
-        addMarkers(data = VelovList_filtered, ~position$longitude, ~position$latitude, label = ~adresse, popup = ~adresse)
+        #addMarkers(data = VelovList_filtered, ~position$longitude, ~position$latitude, label = ~adresse, popup = ~adresse)
+        addMarkers(
+          data = VelovList_filtered, ~position$longitude, ~position$latitude,
+          label = "Station",
+          popup = paste("Adresse: ","<br>",
+                        "Vélos disponibles: ","<br>",
+                        "Places parking total: ","<br>",
+                        "Places disponibles: ","<br>")
+        )
     })
   })
-  ##### Page info station
+  ##### Page info station ######
   
-  observeEvent(input$bouton_recherche, {
-    recherche <- input$recherche
-    
-    # Effectuez la recherche dans le dataframe VelovList
-    resultat_recherche <- VelovList[grepl(recherche, VelovList$name, ignore.case = TRUE), ]
-    
-    # Appelle API pour les stations
-    dataStation = GET("https://api.jcdecaux.com/vls/v3/stations/?contract=lyon&apiKey=ef2b5b3f4ce33805dd75a5192dda54ddd11b5b06")
-    
-    bddStation <- fromJSON ( rawToChar ( dataStation$content ) , flatten =  TRUE )
-    df_station = data.frame(bddStation)
-    df_station = rbind(bddStation, unlist(bddStation))
-    
-    # Récupération des données pour les KPI de la deuxieme page
-    ### 4
-    
-    placeTotalDispo = subset(VelovList$totalStands$availabilitie$stands, VelovList$number == recherche)
-    
-    output$placeDispo_box <- renderText({
-      placeTotalDispo
-    })
-    ### KPI 5
-    
-    velo_meca_dispo = subset(VelovList$totalStands$availabilitie$mechanicalBikes, VelovList$number == recherche)
-    
-    output$VeloMecaDispo_box <- renderText({
-      velo_meca_dispo
-    })
-    
-    ### KPI 6 
-    
-    velo_elec_dispo = subset(VelovList$totalStands$availabilitie$electricalBikes, VelovList$number == recherche)
-    
-    output$VeloElecDispo_box <- renderText({
-      velo_elec_dispo
-  })
-    ### KPI de test pour un grahique 
-    temps_artificiel <- reactiveVal(0)
-    
-    invalidateLater(1000)  # Met à jour toutes les 60 secondes (ajustez selon vos besoins)
-    
-    temps_artificiel(temps_artificiel() + 1)
-   output$graphique_temps_reel <- renderPlot({
-  temps <- 1:temps_artificiel()  # Utilisez les valeurs de temps artificiel comme l'axe x
-  donnees <- subset(VelovList$totalStands$availabilitie$stands, VelovList$number == recherche)  # Récupérez les données en temps réel
+  ####### INTEGRER LA SOLUTION D'IMAD, J'AI DEJA MIS LE FRONT AVEC LES BOX MAIS JE N'AI PAS REUSSI POUR LE BACK #####
   
-  # Créez un graphique en utilisant la fonction plot() de base
-  plot(temps, donnees, type = "l", xlab = "Temps", ylab = "Nombre de places disponibles",
-       main = "Évolution du nombre de vélos places disponibles")
-})
-
-
+    observeEvent(input$bouton_recherche, {
+      recherche <- input$recherche
+      
+      # Effectuez la recherche dans le dataframe VelovList
+      resultat_recherche <- VelovList[grepl(recherche, VelovList$name, ignore.case = TRUE), ]
+      
+      # Appelle API pour les stations
+      dataStation = GET("https://api.jcdecaux.com/vls/v3/stations/?contract=lyon&apiKey=ef2b5b3f4ce33805dd75a5192dda54ddd11b5b06")
+      
+      bddStation <- fromJSON ( rawToChar ( dataStation$content ) , flatten =  TRUE )
+      df_station = data.frame(bddStation)
+      df_station = rbind(bddStation, unlist(bddStation))
+      
+      # Récupération des données pour les KPI de la deuxieme page
+      ### 4
+      
+      placeTotalDispo = subset(VelovList$totalStands$availabilitie$stands, VelovList$number == recherche)
+      
+      output$placeDispo_box <- renderText({
+        placeTotalDispo
+      })
+      ### KPI 5
+      
+      velo_meca_dispo = subset(VelovList$totalStands$availabilitie$mechanicalBikes, VelovList$number == recherche)
+      
+      output$VeloMecaDispo_box <- renderText({
+        velo_meca_dispo
+      })
+      
+      ### KPI 6 
+      
+      velo_elec_dispo = subset(VelovList$totalStands$availabilitie$electricalBikes, VelovList$number == recherche)
+      
+      output$VeloElecDispo_box <- renderText({
+        velo_elec_dispo
+      })
+      ### KPI 7 
+      velo_elec_batterie_removable = subset(VelovList$totalStands$availabilitie$electricalRemovableBatteryBikes, VelovList$number== recherche)
+      
+      output$VeloElecBatterieRemovable_box <- renderText ({
+        velo_elec_batterie_removable
+      })
     
     # Mettez à jour le texte affiché avec les résultats de la recherche
     output$resultat_recherche <- renderText({
@@ -131,10 +156,10 @@ server <- function(input, output) {
         # On trouve la ligne de la station dans le df avec le numéro qui correspond au nom chercher
         stationVelo = VelovList[VelovList$number == resultat_recherche$number, ]
         # Afficher tout les détails de la station
-        stationVelo$totalStands$availabilities$electricalBikes
+        paste()
       } else {
         "Aucun résultat trouvé"
       }
     })
   })
-}
+  }

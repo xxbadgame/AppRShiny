d9713b611ce30454ff91fee686645c00247995de
Imad Farsi <123750555+WaImad@users.noreply.github.com>
2023-10-24 19:38:56 +0200

    Update server.R

--- server.R
+++ server.R
@@ -5,6 +5,8 @@ library(RMySQL)
 library(tidygeocoder)
 library(leaflet)
 library(readr)
+install.packages("readr")
+install.packages("shinyjs")
 library(shinyjs)
 install.packages("ggplot2")
 library(ggplot2)
@@ -20,7 +22,7 @@ server <- function(input, output) {
   
   # Création du dataframe, à la mise en ligne utiliser la base de données
   VelovList <- fromJSON(rawToChar(GET(url)$content))
-
+  
   
   ## DEBLOQUER au démarrage
   #VelovList$adresse<-reverse_geo(lat = VelovList$position$latitude, long = VelovList$position$longitude, method = "osm")
@@ -33,7 +35,7 @@ server <- function(input, output) {
   
   observeEvent(input$bouton_refresh, {
     
-  
+    
     # Afficher l'animation de chargement au début
     shinyjs::show("loading_animation")
     
@@ -42,7 +44,7 @@ server <- function(input, output) {
     
     # Masquer l'animation de chargement après le traitement
     shinyjs::hide("loading_animation")
-  
+    
   })
   
   ### 1 
@@ -74,7 +76,7 @@ server <- function(input, output) {
   
   ##### La carte ######
   observe({
-  
+    
     num_stations <- input$nombre_stations
     
     # Filtrer les données en fonction du nombre de stations sélectionné
@@ -104,48 +106,87 @@ server <- function(input, output) {
   
   ####### INTEGRER LA SOLUTION D'IMAD, J'AI DEJA MIS LE FRONT AVEC LES BOX MAIS JE N'AI PAS REUSSI POUR LE BACK #####
   
-    observeEvent(input$bouton_recherche, {
-      recherche <- input$recherche
+  observeEvent(input$bouton_recherche, {
+    recherche <- input$recherche
+    
+    # Effectuez la recherche dans le dataframe VelovList
+    resultat_recherche <- VelovList[grepl(recherche, VelovList$name, ignore.case = TRUE), ]
+    
+    # Appelle API pour les stations
+    dataStation = GET("https://api.jcdecaux.com/vls/v3/stations/?contract=lyon&apiKey=ef2b5b3f4ce33805dd75a5192dda54ddd11b5b06")
+    
+    bddStation <- fromJSON ( rawToChar ( dataStation$content ) , flatten =  TRUE )
+    df_station = data.frame(bddStation)
+    df_station = rbind(bddStation, unlist(bddStation))
+    
+    # Récupération des données pour les KPI de la deuxieme page
+    ### 4
+    
+    placeTotalDispo = subset(VelovList$totalStands$availabilitie$stands, VelovList$number == recherche)
+    
+    output$placeDispo_box <- renderText({
+      placeTotalDispo
+    })
+    ### KPI 5
+    
+    velo_meca_dispo = subset(VelovList$totalStands$availabilitie$mechanicalBikes, VelovList$number == recherche)
+    
+    output$VeloMecaDispo_box <- renderText({
+      velo_meca_dispo
+    })
+    
+    ### KPI 6 
+    
+    velo_elec_dispo = subset(VelovList$totalStands$availabilitie$electricalBikes, VelovList$number == recherche)
+    
+    output$VeloElecDispo_box <- renderText({
+      velo_elec_dispo
+    })
+    ### Filtres 
+    function(input, output, session) {
+      output$out6 <- renderPrint(input$in6)
+    }
+    donnees_filtrees_reactive <- reactive({
+      # Obtenez les stations sélectionnées à partir du filtre (input$in6) comme vecteur de chaînes de caractères
+      stations_selectionnees <- input$in6
       
-      # Effectuez la recherche dans le dataframe VelovList
-      resultat_recherche <- VelovList[grepl(recherche, VelovList$name, ignore.case = TRUE), ]
+      # Filtrer les données VelovList en fonction des stations sélectionnées
+      VelovList[VelovList$name %in% stations_selectionnees, ]
+    })
+    
+    output$graphique_dynamique_station <- renderPlot({
+      donnees_filtrees <- donnees_filtrees_reactive()
       
-      # Appelle API pour les stations
-      dataStation = GET("https://api.jcdecaux.com/vls/v3/stations/?contract=lyon&apiKey=ef2b5b3f4ce33805dd75a5192dda54ddd11b5b06")
+      # Créez un graphique en utilisant ggplot2
+      mon_graphique <<- ggplot(data = donnees_filtrees, aes(x = name)) +
+        geom_bar(aes(y = totalStands$availabilitie$mechanicalBikes, fill = "Mécaniques"), stat = "identity", position = "dodge") +
+        geom_bar(aes(y = totalStands$availabilitie$electricalBikes, fill = "Électriques"), stat = "identity", position = "dodge") +
+        labs(title = "Graphique en Barres Dynamique",
+             x = "Stations",
+             y = "Nombre de Vélos",
+             fill = "Type de Vélos") +
+        scale_fill_manual(values = c("Mécaniques" = "deepskyblue4", "Électriques" = "gray")) +
+        scale_y_continuous(
+          limits = c(0, 15),
+          breaks = seq(0, 15, by = 1),
+          labels = seq(0, 15, by = 1)
+        )
+      print(mon_graphique)
+    })
+    
+    output$exporter_png <- downloadHandler(
+      filename = function() {
+        paste("graphique_dynamique.png")
+      },
+      content = function(file) {
+        # Sauvegardez le graphique en tant que fichier PNG
+        png(file, width = 800, height = 600)
+        print(mon_graphique)  # Remplacez ggplot_graph par le nom de votre objet graphique ggplot2
+        dev.off()
+      }
+    )
+    
       
-      bddStation <- fromJSON ( rawToChar ( dataStation$content ) , flatten =  TRUE )
-      df_station = data.frame(bddStation)
-      df_station = rbind(bddStation, unlist(bddStation))
-      
-      # Récupération des données pour les KPI de la deuxieme page
-      ### 4
-      
-      placeTotalDispo = subset(VelovList$totalStands$availabilitie$stands, VelovList$number == recherche)
-      
-      output$placeDispo_box <- renderText({
-        placeTotalDispo
-      })
-      ### KPI 5
-      
-      velo_meca_dispo = subset(VelovList$totalStands$availabilitie$mechanicalBikes, VelovList$number == recherche)
-      
-      output$VeloMecaDispo_box <- renderText({
-        velo_meca_dispo
-      })
-      
-      ### KPI 6 
-      
-      velo_elec_dispo = subset(VelovList$totalStands$availabilitie$electricalBikes, VelovList$number == recherche)
-      
-      output$VeloElecDispo_box <- renderText({
-        velo_elec_dispo
-      })
-      ### KPI 7 
-      velo_elec_batterie_removable = subset(VelovList$totalStands$availabilitie$electricalRemovableBatteryBikes, VelovList$number== recherche)
-      
-      output$VeloElecBatterieRemovable_box <- renderText ({
-        velo_elec_batterie_removable
-      })
     
     # Mettez à jour le texte affiché avec les résultats de la recherche
     output$resultat_recherche <- renderText({
@@ -162,4 +203,4 @@ server <- function(input, output) {
       }
     })
   })
-  }
+}

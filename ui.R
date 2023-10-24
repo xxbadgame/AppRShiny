f898579a3d6177c32d483ea3b8d33d64345b5383
Imad Farsi <123750555+WaImad@users.noreply.github.com>
2023-10-24 19:39:25 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,6 +3,7 @@ library(shiny)
 library(leaflet)
 library(jsonlite)
 library(httr)
+library(shinyjs)
 
 # Initialisation de l'API
 contract <- "Lyon"
@@ -36,7 +37,7 @@ ui <- dashboardPage(
         
         actionButton("bouton_refresh", "Rafraîchir les données",style = "margin-bottom: 10px;"),
         
-  
+        
         # Élément pour l'animation de chargement
         useShinyjs(),
         extendShinyjs(text = 'shinyjs.init();', functions = c(
@@ -84,7 +85,7 @@ ui <- dashboardPage(
             height = 200
           ),
           
-            ##
+          ##
           
           # Carte en bas à droite en grand (exclu de l'onglet "Utilisateurs")
           
@@ -93,60 +94,74 @@ ui <- dashboardPage(
             div(style = "height: 100%;", leafletOutput("map")),
             width = 8, # Largeur réduite pour faire de la place aux indicateurs
             height = 600 # Ajustez la hauteur en fonction de vos besoins
-          
+            
           )
         )
       ),
       tabItem(tabName = "info_station",
-            includeCSS("www/custom.css"),
-            fluidRow(
-              div(
-                class = "custom-margin",  # Classe CSS pour la marge
-                textInput("recherche", "Chercher une station :", value = "")
+              includeCSS("www/custom.css"),
+              fluidRow(
+                div(
+                  class = "custom-margin",  # Classe CSS pour la marge
+                  textInput("recherche", "Chercher une station :", value = "")
+                ),
+                div(
+                  class = "custom-margin",  # Classe CSS pour la marge
+                  actionButton("bouton_recherche", "Rechercher")
+                ),
+                div(
+                  class = "custom-margin",  # Classe CSS pour la marge
+                  textOutput("resultat_recherche")
+                )
               ),
-              div(
-                class = "custom-margin",  # Classe CSS pour la marge
-                actionButton("bouton_recherche", "Rechercher")
+              fluidRow(
+                box(
+                  # Somme de available_bike
+                  title = "Nombre de places disponibles de la station",
+                  div(style = "text-align: center; font-size: 24px;", textOutput("placeDispo_box")),
+                  width = 4,
+                  height = 150
+                ),
+                box(
+                  # Somme de meca available_bike
+                  title = "Nombre de vélos mécaniques disponibles",
+                  div(style = "text-align: center; font-size: 24px;", textOutput("VeloMecaDispo_box")),
+                  width = 4,
+                  height = 150
+                ),
+                box(
+                  # Somme de elec available_bike
+                  title = "Nombre de vélos éléctriques disponibles",
+                  div(style = "text-align: center; font-size: 24px;", textOutput("VeloElecDispo_box")),
+                  width = 4,
+                  height = 150
+                ),
+                fluidRow(
+                  column(4,
+                         hr(),
+                         verbatimTextOutput('out6'),
+                         selectInput('in6', 'Filtre', VelovList$name, multiple=TRUE, selectize=TRUE),
+                        
+                ),
+                box(
+                  title = "Graphique Dynamique",
+                  width = 12,
+                  plotOutput("graphique_dynamique_station")
+                ),
+                box(
+                  # Bouton d'exportation en PNG
+                  downloadButton("exporter_png", "Exporter en PNG"),
+                  width = 12,
+                )
+                ),
+                
               ),
-              div(
-                class = "custom-margin",  # Classe CSS pour la marge
-                textOutput("resultat_recherche")
-              )
-            ),
-            fluidRow(
-              box(
-                # Somme de available_bike
-                title = "Nombre de places disponibles de la station",
-                div(style = "text-align: center; font-size: 24px;", textOutput("placeDispo_box")),
-                width = 4,
-                height = 150
-              ),
-              box(
-                # Somme de meca available_bike
-                title = "Nombre de vélos mécaniques disponibles",
-                div(style = "text-align: center; font-size: 24px;", textOutput("VeloMecaDispo_box")),
-                width = 4,
-                height = 150
-              ),
-              box(
-                # Somme de elec available_bike
-                title = "Nombre de vélos éléctriques disponibles",
-                div(style = "text-align: center; font-size: 24px;", textOutput("VeloElecDispo_box")),
-                width = 4,
-                height = 150
-              ),
-              box(title = "Nombre de vélo électrique à batterie removable",
-                  div(style = "text-align: center; font-size: 24px;", textOutput("VeloElecBatterieRemovable_box")),
-                  width = 6,
-                  height = 150)
-              ),
-            ),
-        tabItem(tabName = "utilisateurs",
-                h1("Utilisateurs"),
-      )     
       ),
-      
-    )
+      tabItem(tabName = "utilisateurs",
+              h1("Utilisateurs"),
+      )     
+    ),
+    
   )
-
+)
 
